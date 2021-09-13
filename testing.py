79ca6ce29a2b5ad84aa7442f31c39aad6b1de612
Fernando Ochoa O <fochoao@pm.me>
2021-09-13 17:04:46 -0500

    PyQt5 Main Window functioning with an AES GCM 256 bit message.
    PyQt5 Main Window functioning with an AES GCM 256 bit message.

--- testing.py
+++ testing.py
@@ -1,12 +1,31 @@
 import sys, os
 from PySide2.QtWidgets import QApplication, QMainWindow, QLabel
 from PySide2.QtCore import Qt
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+
+backend = default_backend()
+pwd=b"i7uJ7ZDx3O5BAHZWiCV4c4XrES0Jotgm"
+msg=b"Hello World, My name is Fernando!!!! The password is 256 bit long."
+aed=b"saltallovermypassword"
+iv=os.urandom(27)
+cipher=Cipher(algorithms.AES(pwd), modes.GCM(iv), backend=backend)
+e=cipher.encryptor()
+e.authenticate_additional_data(aed)
+ct=e.update(msg) + e.finalize()
+tag=e.tag
+cipher=Cipher(algorithms.AES(pwd), modes.GCM(iv,tag), backend=backend)
+d=cipher.decryptor()
+d.authenticate_additional_data(aed)
+clear=d.update(ct)+d.finalize()
+assert clear,msg
+x = clear.decode()
 
 class VentanaPrincipal(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setWindowTitle("CryptX Chat")
-        label = QLabel("Hello World!")
+        label = QLabel(x)
         label.setAlignment(Qt.AlignCenter)
         self.setCentralWidget(label)
 
